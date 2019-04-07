736221c41caca77324e423aa5717bc08e03ebd61
littlemeadow1994 <47500924+littlemeadow1994@users.noreply.github.com>
2019-04-07 19:14:42 +0200

    Update assignment2.R

--- assignment2.R
+++ assignment2.R
@@ -1,3 +1,6 @@
+# makeCacheMatrix will return a list containing functions to set the values of a square matrix, get the values of a 
+# square matrix, set the values of the inverse of a square matrix and get the values of the inverse of a square matrix. 
+
 makeCacheMatrix <- function(x = matrix()){
         inv <- NULL
         set <- function(y) {
@@ -12,6 +15,11 @@ makeCacheMatrix <- function(x = matrix()){
              getinverse = getinverse)
         
 }
+
+# cacheSolve will calculate the inverse of a the list created with makeCachematrix. It will first check to see if the 
+# inverse has already been calculated. If so, it will get the mean from the cache and skip the calculation of the inverse.
+# Otherwise, it will calculate the inverse of a square matrix and sets the value of the inverse in the cache via setinverse 
+# function. 
 
 cacheSolve <- function(x, ...){
         inv = x$getinverse()
